7e9f805a4af25487b006693b90452ba0d4a739d7
Salah795 <salah.taher4@gmail.com>
2025-05-24 00:31:02 +0300

    starting with SubImgCharMatcher class

---
+++ src/image/SubImgCharMatcher.java
@@ -0,0 +1,20 @@
+package image;
+
+public class SubImgCharMatcher {
+
+    public SubImgCharMatcher(char[] charset) {
+        //TODO implement this method
+    }
+
+    public char getCharByImageBrightness(double brightness) {
+        //TODO implement this method
+    }
+
+    public void addChar(char c) {
+        //TODO implement this method
+    }
+
+    public void removeChar(char c) {
+        //TODO implement this method
+    }
+}
